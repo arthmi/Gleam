8b0299c4c731783c9100ea51a04c3678ea81c464
arthmi <arthur.miroudot@gmail.com>
2026-07-05 12:25:06 +0200

    feat(rainbow module): remove the fps parameter

--- server/modules/rainbow/main.py
+++ server/modules/rainbow/main.py
@@ -7,15 +7,13 @@ from server.api.models import ColorModel
 class RainbowModule(ModuleBase):
     def __init__(self, target, clock, params):
         super().__init__(target, clock, params, layers=[Layer.COLOR])
-        self.every_n_frames = round(1 / params['fps'] * clock.fps)
 
     async def run(self):
         while self.running:
             await self.clock.wait()
             base_hue = (self.clock.frame * self.params['speed'] / self.clock.fps) % 1.0
-            if self.clock.frame % self.every_n_frames == 0:
-                for i in range(self.target.num_leds):
-                    hue = (base_hue + i * self.params['spread'] / self.target.num_leds) % 1.0
-                    r, g, b = colorsys.hsv_to_rgb(hue, 1.0, 1.0)
-                    self._set_color(ColorModel(r=int(r*255), g=int(g*255), b=int(b*255)))
-                self.target.show()
+            for i in range(self.target.num_leds):
+                hue = (base_hue + i * self.params['spread'] / self.target.num_leds) % 1.0
+                r, g, b = colorsys.hsv_to_rgb(hue, 1.0, 1.0)
+                self._set_color(ColorModel(r=int(r*255), g=int(g*255), b=int(b*255)))
+            self.target.show()
